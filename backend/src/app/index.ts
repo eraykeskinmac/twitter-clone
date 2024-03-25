44c51075d1017f982deb2332d6a30951d2832b26
eraykeskinmac <eraykeskinmac@gmail.com>
2024-03-25 21:28:58 +0300

    feat: add Prisma client and update dependencies

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -2,11 +2,13 @@ import { ApolloServer } from "@apollo/server";
 import express from "express";
 import { expressMiddleware } from "@apollo/server/express4";
 import bodyParser from "body-parser";
+import { prismaClient } from "../clients/db";
 
 export async function initServer() {
   const app = express();
 
   app.use(bodyParser.json());
+
   const graphqlServer = new ApolloServer({
     typeDefs: `
     type Query {
